3c61fe0825bfd39dc17e8d441ace9b4f3b90d1f0
roni46 <39713502+roni46@users.noreply.github.com>
2019-01-03 17:47:51 +0700

    Add files via upload

---
+++ tugas program bilangan positif & negatif.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+    printf("PROOGRAM BILANGAN POSITIF DAN NEGATIF\n");
+    printf("BY M Achmad Sahroni NIM 311620782\n");
+    printf("KAMPUS PELITA BANGSA. TEKNIK INFORMATIKA\n\n");
+	int x ;
+	printf("masukan bilangan :");
+	scanf("%d",&x);
+	if(x>=0)
+	{
+		printf("%d adalah bilangan positif\n\n",x);
+
+	}
+	else
+	{
+		printf("%d adalah bilangan negatif\n\n",x);
+	}
+	printf("PAUSE");
+}
